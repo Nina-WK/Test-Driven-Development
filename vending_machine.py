d7545f414629350e76bfcf76476ba33c1e5c397d
Nikolina Weg Krsticevic <nina.weg@gmail.com>
2018-08-11 17:59:52 +0000

    The amount required is represented by a single coin

--- vending_machine.py
+++ vending_machine.py
@@ -1,8 +1,12 @@
 from byotest import *
 
 def get_change(amount):
+    if amount == 0:
         return []
+        
+    return [1]
 
 test_are_equal(get_change(0),[])
+test_are_equal(get_change(1), [1])
 
 print("All tests pass!")
