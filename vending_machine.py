9324cf38108d722d985ca1f8d37be9d3ff98406d
Nikolina Weg Krsticevic <nina.weg@gmail.com>
2018-08-11 18:24:18 +0000

    Test for more specific amount of change added

--- vending_machine.py
+++ vending_machine.py
@@ -7,7 +7,12 @@ def get_change(amount):
     if amount in [100, 50, 20, 10, 5, 2, 1]:
         return [amount]
         
-    return [2,1]
+    change = []
+    for coin in [100, 50, 20, 10, 5, 2, 1]:
+        if coin <= amount:
+            amount -= coin
+            change.append(coin)
+    return change
 
 test_are_equal(get_change(0),[])
 test_are_equal(get_change(1),[1])
@@ -18,5 +23,6 @@ test_are_equal(get_change(20),[20])
 test_are_equal(get_change(50),[50])
 test_are_equal(get_change(100),[100])
 test_are_equal(get_change(3),[2,1])
+test_are_equal(get_change(7),[5,2])
 
 print("All tests pass!")
